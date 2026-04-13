2c0f5a9691909eefc7173d351bcab68e6da6a460
pixelThreader <rana.workslance@gmail.com>
2026-04-13 23:39:07 +0530

    feat: registered the rest api urls to the server.py

--- backend/server.py
+++ backend/server.py
@@ -15,6 +15,7 @@ from main.apis.bucket.bucket_urls import router as bucket_router
 from main.apis.chats.chat_urls import router as chats_router
 from main.apis.history.history_urls import router as history_router
 import main.apis.reasearch.research_urls as research_urls
+from main.apis.research_static.research_static_urls import router as research_static_router
 from main.apis.settings.settings_urls import router as settings_router
 from main.apis.search.search_utls import router as search_router
 from main.apis.workspace.workspace_urls import router as workspace_router
@@ -180,6 +181,7 @@ app.include_router(chats_router)
 app.include_router(bucket_router)
 app.include_router(settings_router)
 app.include_router(search_router)
+app.include_router(research_static_router)
 
 if __name__ == "__main__":
     uvicorn.run("server:app", host="0.0.0.0", port=8000, workers=1)
